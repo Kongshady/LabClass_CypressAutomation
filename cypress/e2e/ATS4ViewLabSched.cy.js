46ffa7e136ac2a86e23cff4650702efb89c82e35
Mikko Jardenico <118814666+Kongshady@users.noreply.github.com>
2024-05-08 14:22:15 +0800

    Add files via upload

--- cypress/e2e/ATS4ViewLabSched.cy.js
+++ cypress/e2e/ATS4ViewLabSched.cy.js
@@ -1,37 +1,58 @@
-describe('template spec', () => {
+describe('View Laboratory Schedule', () => {
 
   it('View All Laboratory Schedules', () => {
-    cy.visit('http://localhost:5173/adminDashboard')
+    cy.visit('http://localhost:5173/admin')
+    cy.get('.p-inputtext').eq(0).type('2001')
+    cy.get('.p-inputtext').eq(1).type('admin')
+    cy.get('.loginButton').click()
     cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('Filter Schedules by Room', () => {
-    cy.visit('http://localhost:5173/adminDashboard')
+    cy.visit('http://localhost:5173/admin')
+    cy.get('.p-inputtext').eq(0).type('2001')
+    cy.get('.p-inputtext').eq(1).type('admin')
+    cy.get('.loginButton').click()
     cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('Filter schedules by Day', () => {
-    cy.visit('http://localhost:5173/adminDashboard')
+    cy.visit('http://localhost:5173/admin')
+    cy.get('.p-inputtext').eq(0).type('2001')
+    cy.get('.p-inputtext').eq(1).type('admin')
+    cy.get('.loginButton').click()
     cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('Search for Specific Lab Schedule', () => {
-    cy.visit('http://localhost:5173/adminDashboard')
+    cy.visit('http://localhost:5173/admin')
+    cy.get('.p-inputtext').eq(0).type('2001')
+    cy.get('.p-inputtext').eq(1).type('admin')
+    cy.get('.loginButton').click()
     cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('View Upcoming Laboratory Schedules', () => {
-    cy.visit('http://localhost:5173/adminDashboard')
+    cy.visit('http://localhost:5173/admin')
+    cy.get('.p-inputtext').eq(0).type('2001')
+    cy.get('.p-inputtext').eq(1).type('admin')
+    cy.get('.loginButton').click()
     cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('View Empty Laboratory Schedule', () => {
-    cy.visit('http://localhost:5173/adminDashboard')
+    cy.visit('http://localhost:5173/admin')
+    cy.get('.p-inputtext').eq(0).type('2001')
+    cy.get('.p-inputtext').eq(1).type('admin')
+    cy.get('.loginButton').click()
     cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('View Detailed Schedule Information', () => {
-    cy.visit('http://localhost:5173/adminDashboard')
+    cy.visit('http://localhost:5173/admin')
+    cy.get('.p-inputtext').eq(0).type('2001')
+    cy.get('.p-inputtext').eq(1).type('admin')
+    cy.get('.loginButton').click()
     cy.get('.menuLabel').contains('Schedule').click()
   })
 
--- cypress/e2e/IST7ViewLabSched.cy.js
+++ cypress/e2e/IST7ViewLabSched.cy.js
@@ -1,29 +1,64 @@
 describe('View Laboratory Schedule', () => {
   it('View Available Labs', () => {
     cy.visit('http://localhost:5173/')
+    cy.get('#button-login').click()
+    cy.get('.p-inputtext').eq(0).type('100002')
+    cy.get('.p-inputtext').eq(1).type('miko1524')
+    cy.get('.loginButton').click()
+    cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('Lab Details', () => {
     cy.visit('http://localhost:5173/')
+    cy.get('#button-login').click()
+    cy.get('.p-inputtext').eq(0).type('100002')
+    cy.get('.p-inputtext').eq(1).type('miko1524')
+    cy.get('.loginButton').click()
+    cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('Filter by Availability', () => {
     cy.visit('http://localhost:5173/')
+    cy.get('#button-login').click()
+    cy.get('.p-inputtext').eq(0).type('100002')
+    cy.get('.p-inputtext').eq(1).type('miko1524')
+    cy.get('.loginButton').click()
+    cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('Day Navigation', () => {
     cy.visit('http://localhost:5173/')
+    cy.get('#button-login').click()
+    cy.get('.p-inputtext').eq(0).type('100002')
+    cy.get('.p-inputtext').eq(1).type('miko1524')
+    cy.get('.loginButton').click()
+    cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('Week Navigation', () => {
     cy.visit('http://localhost:5173/')
+    cy.get('#button-login').click()
+    cy.get('.p-inputtext').eq(0).type('100002')
+    cy.get('.p-inputtext').eq(1).type('miko1524')
+    cy.get('.loginButton').click()
+    cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('Time Slot Visibility', () => {
     cy.visit('http://localhost:5173/')
+    cy.get('#button-login').click()
+    cy.get('.p-inputtext').eq(0).type('100002')
+    cy.get('.p-inputtext').eq(1).type('miko1524')
+    cy.get('.loginButton').click()
+    cy.get('.menuLabel').contains('Schedule').click()
   })
 
   it('Schedule Confilct Warning', () => {
     cy.visit('http://localhost:5173/')
+    cy.get('#button-login').click()
+    cy.get('.p-inputtext').eq(0).type('100002')
+    cy.get('.p-inputtext').eq(1).type('miko1524')
+    cy.get('.loginButton').click()
+    cy.get('.menuLabel').contains('Schedule').click()
   })
 })
